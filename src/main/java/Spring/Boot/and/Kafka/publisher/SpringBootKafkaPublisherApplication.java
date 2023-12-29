d53322818c2410ae8c2aad3f8cfd6fd30987e529
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2023-12-29 12:03:15 +0100

    Initial commit

---
+++ src/main/java/Spring/Boot/and/Kafka/publisher/SpringBootKafkaPublisherApplication.java
@@ -0,0 +1,13 @@
+package Spring.Boot.and.Kafka.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring/Boot/and/Kafka/publisher/SpringBootKafkaPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Boot.and.Kafka.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
