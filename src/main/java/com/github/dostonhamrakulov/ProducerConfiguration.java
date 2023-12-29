6c3a3b659155bba879eca4efb189d4eaaf3b2d99
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2023-12-29 12:36:27 +0100

    Refactoring ProducerConfiguration and use properties file

--- src/main/java/com/github/dostonhamrakulov/ProducerConfiguration.java
+++ src/main/java/com/github/dostonhamrakulov/ProducerConfiguration.java
@@ -1,6 +1,5 @@
 package com.github.dostonhamrakulov;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +7,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -20,9 +20,9 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
-
 /**
  * Kafka Producer Configuration
+ *
  * @author Doston Hamrakulov
  */
 
@@ -30,85 +30,75 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 public class ProducerConfiguration {
 
-    private static final String LOCALHOST="127.0.0.1:29092";
+    @Value("kafka.config.hostname")
+    private String hostname;
+
+    @Value("kafka.config.groupIdConfig")
+    private String groupIdConfig;
 
     @Bean
-    public ProducerFactory<String, UserDto> producerFactory()
-    {
-        Map<String , Object> userConfig = new HashMap<>();
+    public ProducerFactory<String, UserDto> producerFactory() {
+        Map<String, Object> userConfig = new HashMap<>();
 
-        userConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:29092");
-        userConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
-        userConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,JsonSerializer.class);
+        userConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, hostname);
+        userConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        userConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(userConfig);
     }
 
     @Bean
-    public ProducerFactory<String, String> producerFactorySS()
-    {
-        Map<String , Object> userConfig = new HashMap<>();
+    public ProducerFactory<String, String> producerFactorySS() {
+        Map<String, Object> userConfig = new HashMap<>();
 
-        userConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:29092");
-        userConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
-        userConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+        userConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, hostname);
+        userConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        userConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(userConfig);
     }
 
     @Bean
-    public ConsumerFactory<String, String>consumerFactorySS()
-    {
-        Map<String,Object> userConfig = new HashMap<>();
-        userConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,LOCALHOST );
-        userConfig.put(ConsumerConfig.GROUP_ID_CONFIG,"Consumer_Group");
-        userConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
-        userConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
+    public ConsumerFactory<String, String> consumerFactorySS() {
+        Map<String, Object> userConfig = new HashMap<>();
+        userConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, hostname);
+        userConfig.put(ConsumerConfig.GROUP_ID_CONFIG, groupIdConfig);
+        userConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        userConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(userConfig);
     }
 
     @Bean
-    public ConsumerFactory<String, UserDto>consumerFactory()
-    {
-        Map<String,Object> userConfig = new HashMap<>();
-        userConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,LOCALHOST );
-        userConfig.put(ConsumerConfig.GROUP_ID_CONFIG,"Consumer_Group");
-        userConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
-        userConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,JsonDeserializer.class);
+    public ConsumerFactory<String, UserDto> consumerFactory() {
+        Map<String, Object> userConfig = new HashMap<>();
+        userConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, hostname);
+        userConfig.put(ConsumerConfig.GROUP_ID_CONFIG, groupIdConfig);
+        userConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        userConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(userConfig);
     }
 
-
-
     @Bean
-    public KafkaTemplate<String, UserDto> kafkaTemplateSS()
-    {
-        return new KafkaTemplate<>(
-                producerFactory());
+    public KafkaTemplate<String, UserDto> kafkaTemplateSS() {
+        return new KafkaTemplate<>(producerFactory());
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplateSU()
-    {
-        return new KafkaTemplate<>(
-                producerFactorySS());
+    public KafkaTemplate<String, String> kafkaTemplateSU() {
+        return new KafkaTemplate<>(producerFactorySS());
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory()
-    {
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory((consumerFactorySS()));
         return factory;
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, UserDto> kafkaListenerContainerFactorySO()
-    {
+    public ConcurrentKafkaListenerContainerFactory<String, UserDto> kafkaListenerContainerFactorySO() {
         ConcurrentKafkaListenerContainerFactory<String, UserDto> factorySO = new ConcurrentKafkaListenerContainerFactory<>();
         factorySO.setConsumerFactory((consumerFactory()));
         return factorySO;
     }
-
-
 }
